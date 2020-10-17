2477384bd84c4cda679775e9d0cf8e2feba62090
Adam H. James <ajames@technomics.net>
2020-10-17 11:23:38 -0400

    added devops build script

--- tools/package_config.R
+++ tools/package_config.R
@@ -78,15 +78,21 @@ rnomics::use_badge_version()
 
 devtools::load_all()
 
-devtools::install(build_vignettes = TRUE)
-
-build_path <- file.path(setupr::get_dirs()$git_local, "costverse", "_builds")
-r_version <- paste(R.version$major, strsplit(R.version$minor, ".", fixed = TRUE)[[1L]][1L], sep = ".")
-
-devtools::build(binary = TRUE, path = file.path(build_path, "bin", r_version))
-devtools::build(path = file.path(build_path, "src"))
-
 detach("package:readflexfile", unload = TRUE)
+
+## ===== Build =====
+
+build_path_root <- file.path(setupr::get_dirs()$git_local, "costverse", "_builds")
+build_path <- list(bin = file.path(build_path_root, "bin", rnomics::r_version()),
+                   src = file.path(build_path_root, "src"))
+
+fs::dir_create(unlist(build_path))
+
+bin_build_file <- devtools::build(binary = TRUE, path = build_path$bin)
+src_build_file <- devtools::build(path = build_path$src)
+
+drat_repo <- file.path(setupr::get_dirs()$git_local, "costverse", "repo")
+rnomics::add_to_drat(c(bin_build_file, src_build_file), drat_repo)
 
 ## ===== Scratch Work =====
 
