9f76a7b35c2273e0b7e0668fe8f1f72ec4b35eb0
Adam H. James <ajames@technomics.net>
2022-01-22 14:42:04 -0500

    updates to sfc_mapping

--- R/sfc_mapping.R
+++ R/sfc_mapping.R
@@ -2,12 +2,13 @@
 #'
 #' A data frame containing the mapping between standard and detailed categories.
 #'
-#' @format \code{sfc_mapping} is a data frame with 26 rows and 4 variables:
+#' @format \code{sfc_mapping} is a data frame with 26 rows and 5 variables:
 #' \describe{
-#'   \item{standard_category_id}{The standard category identifier}
-#'   \item{standard_category}{The standard category}
-#'   \item{detailed_standard_category_id}{The detailed category identifier}
-#'   \item{ direct_or_overhead}{Either "direct" or "overhead"}
+#'   \item{standard_category_id}{The standard category identifier.}
+#'   \item{detailed_standard_category_id}{The detailed category identifier.}
+#'   \item{functional_category}{The overarching functional category grouping.}
+#'   \item{direct_or_overhead}{Either "direct" or "overhead".}
+#'   \item{direct_or_overhead}{Either "labor", "material", or "other".}
 #' }
 #'
 "sfc_mapping"
