87457894df14234d5ce9d6be53e4aa0501f42909
Adam H. James <ajames@technomics.net>
2022-10-24 15:52:10 -0400

    clean up config

--- tools/package_config.R
+++ tools/package_config.R
@@ -120,16 +120,3 @@ flexfile %>%
   snake_to_data_model(flexfile_spec) %>%
   data_model_to_snake(flexfile_spec)
 
-# ff_dir <- "C:/Users/ajames/OneDrive - Technomics/Stryker/Task 0003 - Stryker CSDR and Contracts CY21-25/Technical/Validation/57290_STS_0046/GDLS FF Outputs/9-21-2021"
-# file <- list.files(ff_dir, full.names = TRUE)
-
-#############
-z <- change_case(flexfile, flexfile_spec, "snake")
-
-change_case(z, flexfile_spec, NULL, "snake", FALSE)
-
-ff_3pt <- change_case(flexfile, flexfile_spec, "snake", "excel_3part")
-
-split_3pt_tables <- flexfile_spec$tables %>%
-  split(.$excel_3part) %>%
-  purrr::map(~ ff_3pt[.x$excel_3part_table])
