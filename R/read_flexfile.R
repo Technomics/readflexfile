3d793ee3500be4fb09ccd3b78203d9494910b022
Adam H. James <ajames@technomics.net>
2024-07-09 18:12:04 -0400

    Removed depr warning

--- R/read_flexfile.R
+++ R/read_flexfile.R
@@ -45,13 +45,13 @@ read_flexfile <- function(file,
                           .warn_utf8_bom = FALSE,
                           .data_case = c("native", "snake")) {
 
-  lifecycle::deprecate_warn(
-    when = "0.5.0",
-    what = I('The default of returning tables and fields using "snake_case"'),
-    with = I('the new names in any new code and we strongly advise refactoring any existing code.
-    Note that new names align with those used in the native report.\n\nIf you must use the old naming
-    in the interim, please use the argument `.data_case = "snake"` or use `costmisc::native_to_snake_case()`')
-  )
+  # lifecycle::deprecate_warn(
+  #   when = "0.5.0",
+  #   what = I('The default of returning tables and fields using "snake_case"'),
+  #   with = I('the new names in any new code and we strongly advise refactoring any existing code.
+  #   Note that new names align with those used in the native report.\n\nIf you must use the old naming
+  #   in the interim, please use the argument `.data_case = "snake"` or use `costmisc::native_to_snake_case()`')
+  # )
 
   # check the file type
   file_type <- check_filetype(file)
